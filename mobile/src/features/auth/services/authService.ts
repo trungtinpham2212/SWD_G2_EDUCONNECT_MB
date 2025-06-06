b2a5c5dbb5d3bd3bbe91d1919c9a8fd5658a88b5
HuynhNgocKhanh <huynhngockhanh1201@gmail.com>
2025-06-06 22:18:59 +0700

    nothing

--- mobile/src/features/auth/services/authService.ts
+++ mobile/src/features/auth/services/authService.ts
@@ -2,11 +2,11 @@ import { FormLoginRequest, LoginResponse } from '@/features/auth/types/index';
 
 export const loginService = async(data:FormLoginRequest): Promise<LoginResponse> =>{
     if(data.username === 'admin' && data.password === '@a'){
-        const jwt = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.FgLPDZNwucp5QaFBkZkt2njGNYslHbbHuR9TGkt4eak';
+        const kwt = 'tooooken';
         const res: LoginResponse = {
             success: true,
             data: {
-                token: jwt,
+                token: kwt,
                 userId: '12345',
                 roleId: 1
             }
